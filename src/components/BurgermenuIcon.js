59163900a80f89cc13b3801604f7522bb2fd04f2
pesonn <43933988+pesonn@users.noreply.github.com>
2020-03-13 17:59:11 +0100

    Added: Function to set crossed Class

--- src/components/BurgermenuIcon.js
+++ src/components/BurgermenuIcon.js
@@ -3,14 +3,32 @@ import React, { useState, useEffect } from "react";
 import "../styles/burgermenuicon.css";
 
 function BurgermenuIcon(props) {
+  const [crossed, setCrossed] = useState(false);
+
+  function toggleCross() {
+    if (crossed === false) {
+      setCrossed(true);
+      const lineclass = document.querySelectorAll(".burgermenu--line");
+      for (let i = 0; i < lineclass.length; i++) {
+        lineclass[i].classList.add("crossed");
+      }
+    } else {
+      setCrossed(false);
+      const lineclass = document.querySelectorAll(".burgermenu--line");
+      for (let i = 0; i < lineclass.length; i++) {
+        lineclass[i].classList.remove("crossed");
+      }
+    }
+  }
+
   return (
     <div
       className="burgermenu burgermenu--button burgermenu--button__togglenav"
-      onClick={props.toggleNav}
+      onClick={(props.toggleNav, toggleCross)}
     >
-      <span class="burgermenu--line burgermenu--line__top"></span>
-      <span class="burgermenu--line burgermenu--line__mid"></span>
-      <span class="burgermenu--line burgermenu--line__bot"></span>
+      <span className="burgermenu--line burgermenu--top"></span>
+      <span className="burgermenu--line burgermenu--mid"></span>
+      <span className="burgermenu--line burgermenu--bot"></span>
     </div>
   );
 }
