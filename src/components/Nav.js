500973b71eddebb77a235554289336da8648bccd
pesonn <43933988+pesonn@users.noreply.github.com>
2020-03-13 15:11:01 +0100

    Added: Hover Effekt

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Footer from "./Footer";
 
-function Nav() {
-  const styles = {
-    position: "absolut",
-    top: "0px",
-    left: "0px",
-    zIndex: "999999",
-    height: "100vh",
-    width: "100vw",
-    backgroundColor: "yellow",
-    overflow: "hidden",
-  };
+import "../styles/navoverlay.css";
 
+function Nav() {
   return (
-    <nav style={styles}>
+    <nav>
       <ul className="mainnav">
         <li className="mainnav_item">
           <a href="/">Home</a>
@@ -22,10 +13,14 @@ function Nav() {
         <li className="mainnav_item">
           <a href="/drinks">Drinks</a>
         </li>
-        <li className="mainnav_item">Location</li>
-        <li className="mainnav_item">About</li>
+        <li className="mainnav_item">
+          <a href="/#">Location</a>
+        </li>
+        <li className="mainnav_item">
+          <a href="#">About</a>
+        </li>
       </ul>
-      <Footer />
+      {/* <Footer /> */}
     </nav>
   );
 }
